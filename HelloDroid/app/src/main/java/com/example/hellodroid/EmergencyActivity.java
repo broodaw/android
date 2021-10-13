8612f319b0138a85e5c6b345f20ac296c4c17031
Thomas Waßenberg <thomas.wassenberg@gmail.com>
2021-10-13 12:56:09 +0200

    update with GPS, but not working properly yet ...

--- HelloDroid/app/src/main/java/com/example/hellodroid/EmergencyActivity.java
+++ HelloDroid/app/src/main/java/com/example/hellodroid/EmergencyActivity.java
@@ -1,19 +1,46 @@
 package com.example.hellodroid;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
 
-public class EmergencyActivity extends AppCompatActivity {
+import java.util.List;
+
+public class EmergencyActivity extends AppCompatActivity implements LocationListener {
 
     private TextView message;
+    private TextView latLon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        latLon = findViewById(R.id.txtLatLon);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager != null && locationManager.getAllProviders().contains(LocationManager.NETWORK_PROVIDER))
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+            }
+
+        if (locationManager != null && locationManager.getAllProviders().contains(LocationManager.GPS_PROVIDER))
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         message = findViewById(R.id.textView);
         ImageView emergencyButton = findViewById(R.id.emergencyButton);
@@ -28,5 +55,35 @@ public class EmergencyActivity extends AppCompatActivity {
 
     private void tapped() {
         message.setText("Your position was sent.");
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        latLon.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
